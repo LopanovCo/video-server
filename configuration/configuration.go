73dff2e725b7a96fadbf01467646d765e2d832bc
Pavel <user7824@gmail.com>
2024-09-09 09:16:29 +0300

    conf: parse archive

--- configuration/configuration.go
+++ configuration/configuration.go
@@ -45,8 +45,19 @@ type HLSConfiguration struct {
 
 // ArchiveConfiguration is a archive configuration for every stream with enabled archive option
 type ArchiveConfiguration struct {
-	MsPerSegment int64  `json:"ms_per_file"`
-	Directory    string `json:"directory"`
+	MsPerSegment int64         `json:"ms_per_file"`
+	Directory    string        `json:"directory"`
+	Minio        MinioSettings `json:"minio_settings"`
+}
+
+// MinioSettings
+type MinioSettings struct {
+	Host          string `json:"host"`
+	Port          int32  `json:"port"`
+	User          string `json:"user"`
+	Password      string `json:"password"`
+	DefaultBucket string `json:"default_bucket"`
+	Path          string `json:"path"`
 }
 
 // CORSConfiguration is settings for CORS
@@ -75,6 +86,8 @@ type StreamArchiveConfiguration struct {
 	Enabled      bool   `json:"enabled"`
 	MsPerSegment int64  `json:"ms_per_file"`
 	Directory    string `json:"directory"`
+	TypeArchive  string `json:"type"`
+	MinioBucket  string `json:"minio_bucket"`
 }
 
 const (
